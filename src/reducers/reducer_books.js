7d5259cbff6fa5f8526fdceea665eb3d0fcdd226
Steven Thomason <thomason84@hotmail.com>
2018-11-10 19:17:02 -0500

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Parts", pages: 101 },
+    { title: "The Jungle Book", pages: 234 },
+    { title: "Tron", pages: 543 },
+    { title: "The Restaurant at the end of the Universe", pages: 999 }
+  ];
+}
